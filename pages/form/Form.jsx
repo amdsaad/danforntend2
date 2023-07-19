aec3b7de2980bc1f9ff91eb9c6a9a90e08167478
amdsaad <saadamd@gmail.com>
2023-07-19 18:01:45 +0300

    form to backend

--- pages/form/Form.jsx
+++ pages/form/Form.jsx
@@ -1,30 +1,30 @@
-import { useRef, useState, useCallback, useEffect } from "react";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import {useForm} from 'react-hook-form'
-import { useTranslation } from "next-i18next";
-import config from "../../components/config";
+import { useRef, useState, useCallback, useEffect } from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'next-i18next';
+import config from '../../components/config';
 const apiURL = config.api_url;
-import axios from "axios";
-import { useRouter } from "next/router";
-import * as yup from 'yup'
-import {yupResolver} from '@hookform/resolvers/yup'
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 export default function Form() {
   const router = useRouter();
-  const [title, setTitle] = useState("");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [city, setCity] = useState("");
-  const [message, setMessage] = useState("");
-  const [feedback, setFeedback] = useState("");
+  const [title, setTitle] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [city, setCity] = useState('');
+  const [message, setMessage] = useState('');
+  const [feedback, setFeedback] = useState('');
 
   const getContact = useCallback(async () => {
     try {
       await axios
         .get(`${apiURL}/settings`, {
           headers: {
-            "Accept-Language": `${router.locale === "en" ? "en" : "ar"}`,
+            'Accept-Language': `${router.locale === 'en' ? 'en' : 'ar'}`,
           },
         })
         .then((response) => {
@@ -41,60 +41,150 @@ export default function Form() {
     } catch (error) {
       console.log(error);
     }
-  }, [setTitle, setName, setEmail, setMobile, setCity, setMessage, router.locale]);
+  }, [
+    setTitle,
+    setName,
+    setEmail,
+    setMobile,
+    setCity,
+    setMessage,
+    router.locale,
+  ]);
 
   const { t } = useTranslation();
 
   useEffect(() => {
     getContact();
   }, [getContact]);
- 
-  const Input_Classes = "border-none w-full py-3 text-[19px] bg-opacity-40 placeholder:text-[19px] px-2 outline-none bg-[#E5E6E7] text-[#552a0eb3] placeholder:text-[#552a0eb3] thin";
-  const schema =  yup.object().shape({
+
+  const Input_Classes =
+    'border-none w-full py-3 text-[19px] bg-opacity-40 placeholder:text-[19px] px-2 outline-none bg-[#E5E6E7] text-[#552a0eb3] placeholder:text-[#552a0eb3] thin';
+  const schema = yup.object().shape({
     name: yup.string().required('required'),
     email: yup.string().email().required('required'),
     city: yup.string().required('required'),
-    message:yup.string().required('required'),
-  })
-  const {register, handleSubmit, formState:{errors}} = useForm({
-    resolver:yupResolver(schema)
-  })
-  const onSubmit = (data)=>{
-    console.log(data)
-  }
+    message: yup.string().required('required'),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    console.log(data);
+    e.preventDefault();
+    // TODO: send data to from nodemailer
+
+    try {
+      await axios
+        .post(
+          `${apiURL}/feedback`,
+          {
+            name,
+            phone,
+            message,
+            email,
+            field,
+            city,
+          },
+          { headers: { accept: `application/json` } },
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            setCity('');
+            setEmail('');
+            setField('');
+            setMessage('');
+            setName('');
+            setPhone('');
+            toast.success('تم إرسال رسالتك بنجاح!', {
+              position: 'top-right',
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: 'light',
+            });
+          }
+        });
+    } catch (error) {
+      toast.error('برجاء التأكد من إملاء البيانات!', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    }
+  };
 
   return (
     <div className=" relative w-full ">
       <form onSubmit={handleSubmit(onSubmit)}>
-
-      <div className="grid grid-cols-1 gap-x-10 gap-y-4 lg:gap-y-8 lg:grid-cols-4">
-        <div className="lg:col-span-2 relative pt-5">
-          <small className=" text-red-900">{errors.name?.message}</small>
-          <input type="text" className={(Input_Classes)} placeholder={name} {...register('name')}/>
+        <div className="grid grid-cols-1 gap-x-10 gap-y-4 lg:gap-y-8 lg:grid-cols-4">
+          <div className="lg:col-span-2 relative pt-5">
+            <small className=" text-red-900">{errors.name?.message}</small>
+            <input
+              type="text"
+              className={Input_Classes}
+              placeholder={name}
+              {...register('name')}
+            />
+          </div>
+          <div className=" relative  pt-5">
+            <small className=" text-red-900">{errors.email?.message}</small>
+            <input
+              type="text"
+              className={Input_Classes}
+              placeholder={email}
+              {...register('email')}
+            />
+          </div>
+          <div className=" relative  pt-5">
+            <input
+              type="number"
+              className={Input_Classes}
+              placeholder={mobile}
+              {...register('mobile')}
+            />
+          </div>
+          <div className="lg:col-span-2 relative  pt-5">
+            <small className=" text-red-900">{errors.city?.message}</small>
+            <input
+              type="text"
+              className={Input_Classes}
+              placeholder={city}
+              {...register('city')}
+            />
+          </div>
+          <div className="lg:col-span-2 relative  pt-5">
+            <input
+              type="text"
+              className={Input_Classes}
+              placeholder={t('AreaOfInterest')}
+              {...register('AreaOfInterest')}
+            />
+          </div>
+          <div className="lg:col-span-4 relative  pt-5">
+            <small className=" text-red-900">{errors.message?.message}</small>
+            <textarea
+              placeholder={message}
+              className={Input_Classes}
+              {...register('message')}
+            ></textarea>
+          </div>
+          <button className="px-32 py-3 thin bg-[#E5E6E7] text-txt hover:bg-txt hover:text-white  bg-opacity-40 text-lg">
+            {t('send')}
+          </button>
         </div>
-        <div className=" relative  pt-5">
-        <small className=" text-red-900">{errors.email?.message}</small>
-          <input type="text" className={(Input_Classes)} placeholder={email} {...register('email')}/>
-        </div>
-        <div className=" relative  pt-5">
-          <input type="number" className={(Input_Classes)} placeholder={mobile} {...register('mobile')}/>
-        </div>
-        <div className="lg:col-span-2 relative  pt-5">
-        <small className=" text-red-900">{errors.city?.message}</small>
-          <input type="text" className={(Input_Classes)} placeholder={city} {...register('city')}/>
-        </div>
-        <div className="lg:col-span-2 relative  pt-5">
-
-          <input type="text" className={(Input_Classes)} placeholder={t("AreaOfInterest")} {...register('AreaOfInterest')} />
-        </div>
-        <div className="lg:col-span-4 relative  pt-5">
-        <small className=" text-red-900">{errors.message?.message}</small>
-          <textarea placeholder={message} className={(Input_Classes)} {...register('message')} ></textarea>
-        </div>
-        <button className="px-32 py-3 thin bg-[#E5E6E7] text-txt hover:bg-txt hover:text-white  bg-opacity-40 text-lg">{t("send")}</button>
-      </div>
       </form>
-
     </div>
   );
 }
@@ -102,7 +192,7 @@ export default function Form() {
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"])),
+      ...(await serverSideTranslations(locale, ['common'])),
     },
   };
 }
