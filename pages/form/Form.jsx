3aef907966df3de50260f26fc80b391922544f42
Toso <hello@iamtoso.com>
2023-07-20 22:53:57 +0300

    form

--- pages/form/Form.jsx
+++ pages/form/Form.jsx
@@ -13,7 +13,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 export default function Form() {
   const { t } = useTranslation();
   const router = useRouter();
-
+  const [formInput, setFormInput] = useState();
   const [formMsg, setFormMsg] = useState("");
   const [title, setTitle] = useState("");
   const [name, setName] = useState("");
@@ -23,7 +23,31 @@ export default function Form() {
   const [message, setMessage] = useState("");
   const [interest, setInterest] = useState("");
   const [feedback, setFeedback] = useState("");
-  const [submittedData, setSubmittedData] = useState({});
+  const [submittedData, setSubmittedData] = useState();
+
+  const Input_Classes = " custominput border-none w-full py-3 text-[19px] bg-opacity-40 placeholder:text-[19px] px-2 outline-none bg-[#E5E6E7] text-[#552a0eb3] placeholder:text-[#552a0eb3] thin";
+  const schema = yup.object().shape({
+    name: yup.string().required(t("formError.name")),
+    email: yup.string().email().required(t("formError.email")),
+    mobile: yup
+      .number()
+      .transform((value) => (isNaN(value) ? undefined : value))
+      .nullable()
+      .required(t("formError.mobile")),
+    city: yup.string().required(t("formError.city")),
+    interest: yup.string().required(t("formError.area")),
+    resume: yup.mixed(),
+    message: yup.string().required(t("formError.message")),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   const getContact = useCallback(async () => {
     try {
       await axios
@@ -42,88 +66,71 @@ export default function Form() {
             setMessage(response?.data?.text_con);
             setFeedback(response?.data?.feedback);
             setInterest(response?.data?.interest);
-            console.log(response?.data?.interest)
-
+            setFormInput(() => {
+              if (router.route === "/" || router.route === "/contact-us") {
+                return (
+                  <>
+                    <small className=" text-red-900">{errors.interest?.message}</small>
+                    <input type="text" className={Input_Classes} placeholder={interest} {...register("interest")} />
+                  </>
+                );
+              } else {
+                return (
+                  <>
+                    <small className=" text-red-900">{errors.resume?.message}</small>
+                    {/* <label htmlFor="resume" className={Input_Classes} > {t('resume')}</label> */}
+                    <input type="file" className="" placeholder={t("resume")} {...register("resume")} id="resume" />
+                  </>
+                );
+              }
+            });
           }
         });
     } catch (error) {
       console.log(error);
     }
-  }, [setTitle, setName, setEmail, setMobile, setCity, setMessage, router.locale]);
+  }, [setTitle, setName, setEmail, setMobile, setCity, setMessage, interest, router.locale]);
 
   useEffect(() => {
     getContact();
   }, [getContact]);
 
-  const Input_Classes = " custominput border-none w-full py-3 text-[19px] bg-opacity-40 placeholder:text-[19px] px-2 outline-none bg-[#E5E6E7] text-[#552a0eb3] placeholder:text-[#552a0eb3] thin";
-  const schema = yup.object().shape({
-    name: yup.string().required(t("formError.name")),
-    email: yup.string().email().required(t("formError.email")),
-    mobile: yup
-      .number()
-      .transform((value) => (isNaN(value) ? undefined : value))
-      .nullable()
-      .required(t("formError.mobile")),
-    city: yup.string().required(t("formError.city")),
-    AreaOfInterest: yup.string().required(t("formError.area")),
-    resume: yup.mixed(),
-    message: yup.string().required(t("formError.message")),
-  });
-  const {
-    register,
-    handleSubmit,
-    formState,
-    formState: { errors, isSubmitSuccessful },
-  } = useForm({
-    resolver: yupResolver(schema), 
-  });
-  
-  const [formInput, setFormInput] = useState(() => {
-    if (router.route === "/" || router.route === "/contact-us") {
-      return (
-        <>
-          <small className=" text-red-900">{errors.AreaOfInterest?.message}</small>
-          <input type="text" className={Input_Classes} placeholder={interest} {...register("AreaOfInterest")} />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <small className=" text-red-900">{errors.resume?.message}</small>
-          {/* <label htmlFor="resume" className={Input_Classes} > {t('resume')}</label> */}
-          <input type="file" className="" placeholder={t("resume")} {...register("resume")} id="resume" />
-        </>
-      );
-    }
-  });
-
-  const onSubmit = (data) => {
-    setSubmittedData(data)
-    console.log(submittedData)
-    setFormMsg("تم إرسال رسالتك بنجاح!");
-    let inputs = gsap.utils.toArray("form input");
-    let textarea = document.querySelector("form textarea");
-    inputs.forEach((e) => {
-      e.value = "";
-      textarea.value = "";
-    });
-
+  const onSubmit = async (data) => {
+    setSubmittedData(data);
+    console.log(data);
     // TODO: send data to from nodemailer
-    // try {
-    //   await axios
-    //     .post(
-    //       `${apiURL}/feedback`,
-    //       {
-    //         name,
-    //         phone,
-    //         message,
-    //         email,
-    //         field,
-    //         city,
-    //       },
-    //       { headers: { accept: `application/json` } }
-    //     )
+    try {
+      await axios
+        .post(
+          `${apiURL}/feedback`,
+          {
+            name: data.name,
+            phone: data.mobile,
+            message: data.message,
+            email: data.emai,
+            field: data.interest,
+            city: data.city,
+          },
+
+          { headers: { accept: `application/json` } }
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            setFormMsg("تم إرسال رسالتك بنجاح!");
+          }
+        });
+    } catch (error) {
+      console.log(error);
+      setFormMsg("حدث خطأ");
+    }
   };
+
+  // let inputs = gsap.utils.toArray("form input");
+  // let textarea = document.querySelector("form textarea");
+  // inputs.forEach((e) => {
+  //   e.value = "";
+  //   textarea.value = "";
+  // });
 
   return (
     <div className=" relative w-full ">
@@ -152,12 +159,11 @@ export default function Form() {
             <textarea placeholder={message} className={Input_Classes} {...register("message")}></textarea>
           </div>
           <div className="flex flex-wrap col-span-4 justify-between items-center">
-          <button className="px-32 py-3 thin bg-[#E5E6E7] text-txt hover:bg-txt hover:text-white  bg-opacity-40 text-lg">{t("send")}</button>
-          <h3>{isSubmitSuccessful && formMsg}</h3>
+            <button className="px-32 py-3 thin bg-[#E5E6E7] text-txt hover:bg-txt hover:text-white  bg-opacity-40 text-lg">{t("send")}</button>
+            <h3>{isSubmitSuccessful && formMsg}</h3>
           </div>
         </div>
       </form>
-    
     </div>
   );
 }
