be0819b47e53abf370a4f128b04fded623eb0900
amdsaad <saadamd@gmail.com>
2023-07-20 16:33:25 +0300

    transition

--- pages/Layout.jsx
+++ pages/Layout.jsx
@@ -12,28 +12,26 @@ const Layout = ({ children }) => {
 
   const smoother = useContext(SmootherContext);
   const router = useRouter();
-  const transitionDuration = 1;
+  const transitionDuration = 0.5;
   const onPageEnter = () => {
     gsap.to(".transition-animation", {
-      scale: 0,
+      x: '100%',
       duration: transitionDuration,
       ease: "power4.inOut",
-      borderRadius: '100vw ',
+      borderRadius: '0',
       stagger: 0.1,
-      transformOrigin: "bottom",
     });
    
   };
 
   const onPageExit = () => {
     gsap.to(".transition-animation", {
-      scale: 1,
+      x: '0',
       duration: transitionDuration,
       ease: "power4.inOut",
       borderRadius: '0 ',
       stagger: 0.1,
       revesed:true,
-      transformOrigin: "top",
     });
   
   };
@@ -78,7 +76,7 @@ const Layout = ({ children }) => {
     <>
       <div className="transition-animation bg-[#fbe6ce]"></div>
       <div className="transition-animation  bg-blue-400 "></div>
-      <div className="transition-animation  bg-indigo-50"></div>
+      {/* <div className="transition-animation  bg-indigo-50"></div> */}
       <SwitchTransition>
         <Transition key={router.pathname} timeout={transitionDuration * 1000} in={true} appear={true} onEnter={onPageEnter} onExit={onPageExit} mountOnEnter={true} unmountOnExit={true}>
        {/* <div id="smooth-wrapper">
