e2f17b430da5aa8cf03dd38df8bd3ec251e8b51f
amdsaad <saadamd@gmail.com>
2023-09-07 17:49:55 +0300

    fix

--- components/home/Services.js
+++ components/home/Services.js
@@ -181,7 +181,7 @@ export default function Services({ tourisms, title, description, about_1, about_
       <section className="w-full relative " id="advSection" ref={advSection}>
         <div className=" absolute top-0 right-0 flex items-end justify-end scrubElements scrubRight" id="advSection_img_1" ref={advSection_img_1}>
 
-          <img src={tourisms[0]?.images[1]?.url} className="hidden lg:hidden xl:block" alt="" className='w-[708px] h-[702px]' />
+          <img src={tourisms[0]?.images[1]?.url} className="hidden lg:hidden xl:block w-[708px] h-[702px]" alt="" />
           {/* <img src={tourisms[0]?.images[1]?.url} className="hidden lg:block xl:hidden" alt="" className='w-[500px] h-[402px]' />
           <img src={tourisms[0]?.images[1]?.url} className="block lg:hidden" alt="" className='w-[300px] h-[324px]' /> */}
           {/* <Image src={tourisms[0]?.images[1]?.url} className="hidden lg:hidden xl:block" alt="" width={708} height={702} /> */}
