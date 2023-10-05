1c93287f5109ee4587e8ca16b5cae4111e3b13c9
amdsaad <saadamd@gmail.com>
2023-10-05 09:02:18 +0300

    mail server update

--- service/mailService.js
+++ service/mailService.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 import axios from 'axios';
 import config from '../components/config';
 const apiURL = config.api_url;
@@ -6,56 +6,51 @@ const apiURL = config.api_url;
 //-----------------------------------------------------------------------------
 
 const emailSettings = async () => {
-
-    try {
-        return await axios
-            .get(`${apiURL}/settings`, {
-                headers: {
-                    'Accept-Language': `en`,
-                },
-            })
-
-    } catch (error) {
-        console.log(error);
-    }
-
-}
+  try {
+    return await axios.get(`${apiURL}/settings`, {
+      headers: {
+        'Accept-Language': `en`,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 export async function sendMail(subject, toEmail, otpText, attachments) {
-    const { data } = await emailSettings();
-    console.log("mailSettings", data.mail)
+  const { data } = await emailSettings();
+  console.log('mailSettings', data.mail);
 
-    if (data.mail) {
-        const transporter = nodemailer.createTransport({
-            host: data.mail.mail_host,
-            port: 465,
-            secure: true,
-            auth: {
-                user: data.mail.mail_username,
-                pass: data.mail.mail_password,
-            },
-        });
+  if (data.mail) {
+    const transporter = nodemailer.createTransport({
+      host: data.mail.mail_host,
+      port: 587,
+      secure: true,
+      auth: {
+        user: data.mail.mail_username,
+        pass: data.mail.mail_password,
+      },
+    });
 
-        var mailOptions = {
-            from: data.mail.mail_from_address,
-            to: toEmail,
-            subject: subject,
-            text: otpText,
-            attachments: attachments
-        };
-        await new Promise((resolve, reject) => {
-            // send mail
-            transporter.sendMail(mailOptions, (err, response) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(response);
-                    return {
-                        status: 200,
-                        message: "Success",
-                        
-                    }
-                }
-            });
-        });
-    }
+    var mailOptions = {
+      from: data.mail.mail_from_address,
+      to: toEmail,
+      subject: subject,
+      text: otpText,
+      attachments: attachments,
+    };
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+          return {
+            status: 200,
+            message: 'Success',
+          };
+        }
+      });
+    });
+  }
 }
