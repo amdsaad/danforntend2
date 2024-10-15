c63de138717cd74333d59bdf8c57169e226a7b0a
amdsaad <saadamd@gmail.com>
2024-10-15 15:49:09 +0300

    gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Main, NextScript, Head } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,9 +9,25 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html dir={this.props.locale === "ar" ? "rtl" : "ltr"} lang={this.props.locale}>
+      <Html
+        dir={this.props.locale === "ar" ? "rtl" : "ltr"}
+        lang={this.props.locale}
+      >
         <Head>
           <link rel="shortcut icon" href="/image/favicon.ico" />
+          {/* Google Tag Manager */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-WMRHYY2DHY"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WMRHYY2DHY');
+          `}
+          </Script>
         </Head>
         <body>
           <Main />
